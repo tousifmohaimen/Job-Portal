7bea5f5fefd60fff29ef0b4256279e55ef242ea8
tousifmh <mohaimentousif10@gmail.com>
2024-01-22 10:50:50 +0800

    First commit

---
+++ contact.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <!-- CSS Stylesheets -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lib/animate/animate.min.css">
+    <link rel="stylesheet" href="lib/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h1 class="mb-5">Contact For Any Query</h1>
+            </div>
+            <div class="row g-3 justify-content-center align-items-center text-center">
+                <div class="col-12">
+                    <div class="row gy-4 justify-content-center">
+                        <div class="col-md-6">
+                            <h5 class="section-title fa fa-envelope-open text-primary me-2">Email</h5>
+                            <p class="m-0"><i class="fa fa-envelope-open text-primary me-2"></i>InclusiveHiring@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="row gy-4 justify-content-center">
+                        <div class="col-md-6">
+                            <h5 class="section-title fa fa-envelope-open text-primary me-2">Phone</h5>
+                            <p class="m-0"><i class="fa fa-envelope-open text-primary me-2"></i>+60142310056</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="d-flex justify-content-center">
+                        <iframe class="position-relative rounded w-100 h-100"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7973.4702624372485!2d102.2676662727158!3d2.2526261640076304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d1e55ed35e74f9%3A0x2a12767cc52ea9f1!2sTaman%20Bukit%20Beruang%20Utama%2C%20Malacca!5e0!3m2!1sen!2smy!4v1673942728243!5m2!1sen!2smy"
+                            frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
+                            tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+    <script src="js/main.js"></script>
+</body>
+
+</html>
